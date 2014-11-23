31c840990e45d24ebcab0614557de34b5c82ae27
shigemk2 <i.am.shige@gmail.com>
2014-11-23 16:15:40 +0900

    main以外でのdoブロックでの呼び出し

--- random.hs
+++ random.hs
@@ -2,6 +2,11 @@ import System.Random
 
 randAlpha = getStdRandom $ randomR ('A', 'Z')
 
+-- doブロック全体で1つのアクションを構成します。testには引数がないため関数ではなく、アクションが束縛された変数
+test = do
+    r <- randAlpha
+    print r
+
 main = do
   r <- randAlpha
   print r
@@ -9,8 +14,15 @@ main = do
   randAlpha >>= print
 
   -- letを使っても、値を取り出すには依然として<-などを使う必要があります
+  print "-----------------------"
   let r = randAlpha
   r' <- r
   print r'
   print =<< r
   r >>= print
+
+  -- main以外でdoを使って、mainから呼び出せます。
+  print "-----------------------"
+  test
+  test
+  test
