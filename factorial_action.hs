fcb8826b609e6de425256fbf35aaeb90b541e34e
shigemk2 <i.am.shige@gmail.com>
2014-11-24 23:33:11 +0900

    階乗アクションに型実装を追加

--- factorial_action.hs
+++ factorial_action.hs
@@ -1,12 +1,14 @@
 -- returnは合わせる
+fact :: Int -> IO Int
 fact 0 = return 1
 fact n | n > 0 = do
   m <- fact (n - 1)
   return $ n * m
 
 main = do
+  print =<< fact 10
   -- 自動で多倍長にしてくれるからこんな無茶な計算でもよい
-  print =<< fact 10000
+  print =<< fact 100000
   -- マイナスの階乗はマッチしない
-  print =<< fact (-10)
+  -- print =<< fact (-10)
 
