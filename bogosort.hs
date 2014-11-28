2a7b822fc83fc434469c4a29feb8d52b754e83f0
shigemk2 <i.am.shige@gmail.com>
2014-11-28 23:45:08 +0900

    ボゴソート

---
+++ bogosort.hs
@@ -0,0 +1,24 @@
+import System.Random
+
+shuffle [] = return []
+shuffle xs = do
+    n <- getStdRandom $ randomR (0, length xs - 1) :: IO Int
+    xs' <- shuffle $ take n xs ++ drop (n + 1) xs
+    return $ (xs !! n) : xs'
+
+sort [] = True
+sort [x] = True
+sort (x:y:xs)
+  | x < y     = sort (y:xs)
+  | otherwise = False
+
+bogosort xs = do
+  xs' <- shuffle xs
+  if sort xs' == True
+  then return xs'
+  else bogosort xs'
+
+main = do
+    xs <- shuffle [1..9]
+    print xs
+    print =<< bogosort xs
