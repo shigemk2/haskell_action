ed8ae8322b5dfdbac54b4e6c431f272feaaa4231
shigemk2 <i.am.shige@gmail.com>
2014-11-29 16:30:54 +0900

    今度こそクイックソート(アクション)

--- qsort_action.hs
+++ qsort_action.hs
@@ -1,8 +1,8 @@
 qsort []     = return []
 qsort (n:xs) = do
-   lt   <- [x | x <- xs, x <  n]
-   gteq <- [x | x <- xs, x >= n]
-   return qsort lt ++ [n] ++ qsort gteq
+   lt   <- qsort [x | x <- xs, x <  n]
+   gteq <- qsort [x | x <- xs, x >= n]
+   return $ lt ++ [n] ++ gteq
 
 main = do
     print =<< qsort [4, 6, 9, 8, 3, 5, 1, 7, 2]
