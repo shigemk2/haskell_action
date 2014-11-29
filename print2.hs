7f00c80e1dce420e7efd148fccd6b739b0d100ee
shigemk2 <i.am.shige@gmail.com>
2014-11-29 14:20:23 +0900

    暗黙の取り出し 1

---
+++ print2.hs
@@ -0,0 +1,15 @@
+main = do
+    let a = print "hello"
+    -- _ <- a
+    -- _ <- a
+    -- a
+    -- a
+    -- a
+    -- a
+    -- a
+    -- a
+    -- a
+    print =<< a
+    -- let b = 1
+    -- print b
+    -- print $ b
