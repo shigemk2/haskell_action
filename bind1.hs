74f4990c8fb1160d7708028bfd2a164641721680
shigemk2 <i.am.shige@gmail.com>
2014-11-29 15:11:47 +0900

    間違った例を追加

--- bind1.hs
+++ bind1.hs
@@ -4,3 +4,10 @@ main = do
     return 2 >>= inc >>= print
     print =<< inc =<< return 2
     print =<< inc 2
+
+-- この例は2箇所間違っている
+-- 一度アクションを使うと戻り値も渡すのもアクションでなければならない
+-- inc x = x + 1
+
+-- main = do
+--     print $ inc =<< return 2
