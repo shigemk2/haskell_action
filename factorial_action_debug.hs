010271b467b6381ea2239fc4d53b6ebb2e17055e
shigemk2 <i.am.shige@gmail.com>
2014-11-29 16:04:47 +0900

    コメント追加

--- factorial_action_debug.hs
+++ factorial_action_debug.hs
@@ -1,15 +1,16 @@
 -- actionでもいける
 -- putStrLnはprintで代用可能
+-- アクションなので、関数の途中でprintやらputStrLnやらを挟むことも可能
 fact 0 = do
-    putStrLn "fact 0 = 1"
+    print "fact 0 = 1"
     return 1
 fact n | n > 0 = do
     let dbg = "fact " ++ show n ++ " = " ++
               show n ++ " * fact " ++ show (n - 1)
-    putStrLn dbg
+    print dbg
     n' <- fact (n - 1)
     let ret = n * n'
-    putStrLn $ dbg ++ " = " ++ show n ++ " * " ++ show n' ++ " = " ++ show ret
+    print $ dbg ++ " = " ++ show n ++ " * " ++ show n' ++ " = " ++ show ret
     return ret
 
 main = do
