55af932367333fdf462c4c2fc615061534fa279a
shigemk2 <i.am.shige@gmail.com>
2014-11-30 16:18:20 +0900

    とりあえず実装完了

--- brainfuck.hs
+++ brainfuck.hs
@@ -1,14 +1,14 @@
 import Data.Array.IO
 main = do
-    -- let bf = ">+++++++++[<++++++++>-]<.>+++++++[<++++>" ++
-    --          "-]<+.+++++++..+++.[-]>++++++++[<++++>-]<" ++
-    --          ".>+++++++++++[<+++++>-]<.>++++++++[<+++>" ++
-    --          "-]<.+++.------.--------.[-]>++++++++[<++" ++
-    --          "++>-]<+.[-]++++++++++."
+    let bf = ">+++++++++[<++++++++>-]<.>+++++++[<++++>" ++
+             "-]<+.+++++++..+++.[-]>++++++++[<++++>-]<" ++
+             ".>+++++++++++[<+++++>-]<.>++++++++[<+++>" ++
+             "-]<.+++.------.--------.[-]>++++++++[<++" ++
+             "++>-]<+.[-]++++++++++."
     -- let bf = "++++++++++++++++++++++++++++++" ++
     --          "++++++++++++++++++++++++++++++" ++
     --          "++++++++++++."
-    let bf = "+++++++++[++++++++]>."
+    -- let bf = "+++++++++[>++++++++<-]>."
     jmp <- newArray (0, length bf + 1) 0 :: IO (IOUArray Int Int)
     let loops = []
 
@@ -29,6 +29,7 @@ main = do
 
     m <- newArray (0, 30000) 0 :: IO (IOUArray Int Int)
     let scanbf pc r | pc < length bf = do
+          -- print pc
           case bf !! pc of
             '+' -> do
               a <- readArray m r
@@ -38,6 +39,10 @@ main = do
               a <- readArray m r
               writeArray m r (a - 1)
               scanbf (pc + 1) r
+            '>' -> do
+              scanbf (pc + 1) (r + 1)
+            '<' -> do
+              scanbf (pc + 1) (r - 1)
             '.' -> do
               a <- readArray m r
               putChar $ toEnum $ fromIntegral $ a
@@ -47,7 +52,7 @@ main = do
               if a == 0
               then do
                 pc <- readArray jmp pc
-                scanbf pc r
+                scanbf (pc + 1) r
               else do
                 scanbf (pc + 1) r
             ']' -> do
@@ -55,7 +60,7 @@ main = do
               if a /= 0
               then do
                 pc <- readArray jmp pc
-                scanbf (pc - 1) r
+                scanbf pc r
               else do
                 scanbf (pc + 1) r
             _ -> do
@@ -63,38 +68,3 @@ main = do
         scanbf _ _ = return ()
     scanbf 0 0
 
-    -- let jmppush s i | i < length bf && (bf !! i) == '[' = jmppush (i : s) (i + 1)
-    --     jmppush s i | i < length bf = jmppush s (i + 1)
-    --     jmppush s _ = s
-
-    -- let loops = jmppush [] 0
-    -- let start = loops !! 0
-    -- print start
-
-    -- let jmppop s i j | i < length bf && (bf !! i) == ']' = do
-    --       let start = loops !! j
-    --       writeArray s start i
-    --       writeArray s i start
-    --       jmppop s (i + 1) (j + 1)
-    --     jmppop s i j | i < length bf = jmppop s (i + 1) j
-    --     jmppop s _ _ = s
-    -- jmppop jmp 0 0
-    -- print =<< getElems jmp
-
-    -- let brainfuck i | (bf !! i) == '+' = do
-    --       a <- readArray pc 0
-    --       writeArray pc 0 (a + 1)
-    -- let brainfuck i | (bf !! i) == '.' = do
-    --       putChar $ toEnum $ fromIntegral $ readArray pc 0
-    -- let brainfuck _ =  putChar $ toEnum 108
-
-    -- let loop i | i < length bf = do
-    --       print $ bf !! i
-    --       brainfuck i
-    --       loop $ i + 1
-    --     loop _ = return ()
-    -- loop 0
-
-    -- -- putChar 'A'
-    -- print $ fromIntegral $ 72
-    -- putChar $ toEnum $ 72
