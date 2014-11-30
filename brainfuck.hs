177b648bdd82feae1398d89588afc0f1a2bab8e7
shigemk2 <i.am.shige@gmail.com>
2014-11-30 15:03:13 +0900

    ループ[の一部

--- brainfuck.hs
+++ brainfuck.hs
@@ -5,11 +5,36 @@ main = do
     --          ".>+++++++++++[<+++++>-]<.>++++++++[<+++>" ++
     --          "-]<.+++.------.--------.[-]>++++++++[<++" ++
     --          "++>-]<+.[-]++++++++++."
-    let bf = "++++++++++++++++++++++++++++++" ++
-             "++++++++++++++++++++++++++++++" ++
-             "++++++++++++."
-
+    -- let bf = "++++++++++++++++++++++++++++++" ++
+    --          "++++++++++++++++++++++++++++++" ++
+    --          "++++++++++++."
+    let bf = "+++++++++[++++++++-]>."
     pc <- newArray (0, 30000) 0 :: IO (IOUArray Int Int)
+    jmp <- newArray (0, 30000) 0 :: IO (IOUArray Int Int)
+    let loops = []
+
+    let scanbf i | (bf !! i) == '[' = do
+          loops ++ [i]
+          -- a <- readArray pc 0
+          -- writeArray pc 0 (a + 1)
+    -- let scanbf i | (bf !! i) == ']' = do
+    --       putChar $ toEnum $ fromIntegral $ readArray pc 0
+    let scanbf _ =  putChar $ toEnum 108
+
+    let loop i | i < length bf = do
+          print $ bf !! i
+          -- jmp i
+          loop $ i + 1
+        loop _ = return ()
+    loop 0
+
+
+    let jmp s i | i < length bf =
+            if (bf !! i) == '['
+            then jmp (i : s) (i + 1)
+            else jmp s (i + 1)
+        jmp s _ = s
+    print $ jmp [] 0
 
     -- let brainfuck i | (bf !! i) == '+' = do
     --       a <- readArray pc 0
